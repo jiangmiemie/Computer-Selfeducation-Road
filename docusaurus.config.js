7dd504aa56d6f2544eda6be4ac6e86cd7e83705e
蒋洋 <jiangyangcreate@gmail.com>
2023-03-08 23:09:18 +0800

    1.0.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,9 +17,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'jiangmiemie', // Usually your GitHub org/user name.
+  projectName: 'Computer-Selfeducation-Road', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
