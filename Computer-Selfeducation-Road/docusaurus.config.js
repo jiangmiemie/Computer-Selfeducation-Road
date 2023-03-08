61be1dbfcf50bc88d3a5dfcdf41dea55f934477d
蒋洋 <jiangyangcreate@gmail.com>
2023-03-08 22:21:25 +0800

    1.0.1

--- Computer-Selfeducation-Road/docusaurus.config.js
+++ Computer-Selfeducation-Road/docusaurus.config.js
@@ -10,10 +10,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://jiangmiemie.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Computer-Selfeducation-Road/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
